d29c9677bd386507e824cfe32e838ebcda62dddd
deiviz <tangerdream@gmail.com>
2023-02-21 10:52:04 +0100

    remove fetch test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,48 +16,6 @@
 </template>
 
 <script setup>
-/* curl--location--request POST 
-'http://tools.masquemovil.com/ecap-api/upifyBilling/pinRequest' \
---header 'Authorization: Basic TW9iV2l6YXJkczE0NDpCUEFBNGEza1owemVPeFdvUHFQUg==' \
---header 'Content-Type: application/json' \
---header 'Cookie: JSESSIONID=98B3BEA014E71C8B8B06818CB8967ED0' \
---data 
-- raw '{    "MSISDN": "966583628899",    "campaign_id": "4071716733",    "click_id": "12345678",    "source": "Google",    "IdService": "44731",    "IP": "172.18.0.1",    "TransId": "32546844965",    "MNC": "123" */
-
-
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      'Cookie': 'JSESSIONID = 98B3BEA014E71C8B8B06818CB8967ED0',
-      'Authorization': 'Basic TW9iV2l6YXJkczE0NDpCUEFBNGEza1owemVPeFdvUHFQUg=='
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    //redirect: 'follow', // manual, *follow, error
-    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData('http://tools.masquemovil.com/ecap-api/upifyBilling/pinRequest', { 
-   "MSISDN": "966583628899", 
-   "campaign_id": "4071716733", 
-   "click_id": "12345678", 
-   "source": "Google", 
-   "IdService": "44731", 
-   "IP": "172.18.0.1", 
-   "TransId": "32546844965", 
-   "MNC": "123"
- })
-  .then((data) => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
 
 </script>
 
